8e80d072bf00f2c9023790a49cbd19564b6972b3
Tom Hall <tom.hall.90@outlook.com>
2019-09-10 13:11:54 +1000

    updated module 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     render(){
         return(
             <div>
-                <div className="QuizQuestions">{quizData.quiz_questions[0].instruction_text}</div>
+                Quiz
             </div>
             )
     }
